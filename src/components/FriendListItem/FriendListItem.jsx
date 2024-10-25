cf4b17b2734c04025a886e0643ef24d91929c31a
Borodkina Olena <elena.kaminnik@gmail.com>
2024-10-25 21:49:28 +0300

    friend

---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,17 @@
+import s from '../FriendListItem/FriendListItem.module.css'
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+    
+    const statusFriends = isOnline ? s.online : s.offline;
+  return (
+    <div className={s.card}>
+      <img src={avatar} alt="Avatar" width="120" />
+      <p className={s.name}>{name}</p>
+          <p className={`${s.status} ${statusFriends}`}>
+          {isOnline ? 'online' : 'offline'}
+      </p>
+    </div>
+  );
+};
+
+export default FriendListItem;
